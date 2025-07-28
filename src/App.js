641a670567b7140fcfecebc4e2dd5c612916187a
dongsunoh <dongsun.oh@gmail.com>
2025-07-28 16:21:16 +0900

    styled-components (Animations and Pseudo Selectors)  - helper function -> keyframes 사용  - 기존 컴포넌트에서 target 형태로 작성 할 수 있음. const Box = styled.div`span{font-size:36px}`; 형태로 작성 가능  - &:hover 로 숏컷 사용 가능 span:hover랑 동일함.

--- src/App.js
+++ src/App.js
@@ -1,29 +1,54 @@
-import styled from "styled-components";
+import styled, {keyframes} from "styled-components";
 
-const Father = styled.div`
+const Wrapper = styled.div`
     display: flex;
 `;
 
-const Btn = styled.button`
-    color: white;
-    background-color: tomato;
-    border: 0;
-    border-radius: 15px;
+const animation = keyframes`
+    0% {
+        transform: rotate(0deg);
+        border-radius: 0;
+    }
+    50% {
+        border-radius: 100px;
+    }
+    100% {
+        transform: rotate(360deg);
+        border-radius: 0;
+    }
 `;
 
-const Input = styled.input.attrs({required:true, minLength:10})`
+const Box = styled.div`
+    width: 200px;
+    height: 200px;
     background-color: tomato;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    animation: ${animation} 1s linear infinite;
+    
+    span {
+        font-size: 36px;
+        &:hover {
+            font-size: 50px;
+        }
+        &:active {
+            opacity: 0;
+        }
+    }
+    span:hover {
+        font-size: 60px;
+    }
+    
 `;
 
 function App() {
   return (
-    <Father as="header">
-        <Btn>Log in</Btn>
-        <Btn as="a" href="/">Log in</Btn>
-        <Input/>
-        <Input/>
-        <Input/>
-    </Father>
+    <Wrapper>
+      <Box>
+          <span>😂</span>
+      </Box>
+    </Wrapper>
   );
 }
 
