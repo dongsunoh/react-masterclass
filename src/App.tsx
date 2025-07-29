f30d1bf0caab2e139e38351c8160c9c47af950eb
dongsunoh <dongsun.oh@gmail.com>
2025-07-29 19:30:10 +0900

    Typescript (Typing the Props)  - Component의 props에 type을 전달하는 방법    > Typescript interface 선언을 통해 어떤 타입인지 정의.    > 해당 변수가 어떤 타입인지 선언해서 사용 함. function ({bgColor} : CircleProps) or styled.div<ContainerProps>로 사용.

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,12 @@
 import React from 'react';
-import styled from "styled-components";
-
-const Wrapper = styled.div`
-    display: flex;
-    height: 100vh;
-    width: 100vw;
-    justify-content: center;
-    align-items: center;
-    background-color: ${(props:any) => props.theme.backgroundColor};
-`;
-
-const Title = styled.h1`
-    color : ${(props:any) => props.theme.textColor}
-`;
+import Circle from "./Circle";
 
 function App() {
   return (
-    <Wrapper>
-      <Title>Hello</Title>
-    </Wrapper>
+      <div>
+        <Circle bgColor="teal"/>
+        <Circle bgColor="tomato"/>
+      </div>
   );
 }
 
--- src/index.tsx
+++ src/index.tsx
@@ -1,26 +1,13 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
-import {ThemeProvider, DefaultTheme} from "styled-components";
-
-const darkTheme:DefaultTheme = {
-    textColor : "whitesmoke",
-    backgroundColor : "#111"
-}
-
-const lightTheme:DefaultTheme = {
-    textColor : "#111",
-    backgroundColor : "whitesmoke"
-}
 
 const root = ReactDOM.createRoot(document.getElementById('root') as HTMLElement
 );
 
 root.render(
   <React.StrictMode>
-      <ThemeProvider theme={darkTheme}>
-          <App />
-      </ThemeProvider>
+      <App />
   </React.StrictMode>
 );
 
