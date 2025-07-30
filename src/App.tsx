d6a9c54e1baf84a251866cd1045b67774d35ec99
dongsunoh <dongsun.oh@gmail.com>
2025-07-30 13:30:07 +0900

    Typescript (Themes)  - styled.d.ts 생성 하여 styled-components의 Theme 정의 확장  - 오타(실수)로부터 방지할 수 있음.

--- src/App.tsx
+++ src/App.tsx
@@ -1,29 +1,20 @@
 import React, {useState} from 'react';
+import styled from "styled-components";
+
+const Container = styled.div`
+    background-color : ${(props) => props.theme.bgColor};
+`;
+const H1 = styled.h1`
+    color : ${(props) => props.theme.textColor};
+`;
 
 function App() {
 
-    const [username, setUsername] = useState("");
-    const onChange = (event : React.FormEvent<HTMLInputElement>) => {
-        const {currentTarget : {value}} = event;
-        setUsername(value);
-    };
-
-    const onSubmit = (event : React.FormEvent<HTMLFormElement>) => {
-        event.preventDefault(); // 기본 제출 동작 막기
-        console.log("hello " + username);
-    };
-
     return (
         <div>
-          <form onSubmit={onSubmit}>
-              <input
-                  value={username}
-                  onChange={onChange}
-                  type="text"
-                  placeholder="username"
-              />
-              <button>Log in</button>
-          </form>
+            <Container>
+                <H1>Hello</H1>
+            </Container>
         </div>
     );
 }
---
+++ src/styled.d.ts
@@ -0,0 +1,10 @@
+import 'styled-components';
+
+// styled-components의 Theme 정의 확장
+declare module 'styled-components' {
+    export interface DefaultTheme {
+        textColor : string;
+        bgColor : string;
+        btnColor : string;
+    }
+}
