223321302ceb70c7b9816fcd478d99cce82ad713
dongsunoh <dongsun.oh@gmail.com>
2025-07-30 10:25:57 +0900

    Typescript (Forms)  - input onChange, form submit 작성

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,31 @@
-import React from 'react';
-import Circle from "./Circle";
+import React, {useState} from 'react';
 
 function App() {
-  return (
-      <div>
-        <Circle borderColor="yellow" bgColor="teal"/>
-        <Circle bgColor="tomato"/>
-      </div>
-  );
+
+    const [username, setUsername] = useState("");
+    const onChange = (event : React.FormEvent<HTMLInputElement>) => {
+        const {currentTarget : {value}} = event;
+        setUsername(value);
+    };
+
+    const onSubmit = (event : React.FormEvent<HTMLFormElement>) => {
+        event.preventDefault(); // 기본 제출 동작 막기
+        console.log("hello " + username);
+    };
+
+    return (
+        <div>
+          <form onSubmit={onSubmit}>
+              <input
+                  value={username}
+                  onChange={onChange}
+                  type="text"
+                  placeholder="username"
+              />
+              <button>Log in</button>
+          </form>
+        </div>
+    );
 }
 
 export default App;
