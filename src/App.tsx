ebc85fdb5d71b30ebcab8ba36a0a75add44ffb7e
dongsunoh <dongsun.oh@gmail.com>
2025-07-30 14:54:38 +0900

    Crypto Tracker (Setup)  - react-router-dom 설치, react-query 설치    > npm i react-router-dom@5.3.4    > npm i --save-dev @types/react-router-dom    > npm i @tanstack/react-query  - Router 설정  - useParams() 사용 : typescript에 맞게

--- src/App.tsx
+++ src/App.tsx
@@ -1,20 +1,11 @@
-import React, {useState} from 'react';
-import styled from "styled-components";
-
-const Container = styled.div`
-    background-color : ${(props) => props.theme.bgColor};
-`;
-const H1 = styled.h1`
-    color : ${(props) => props.theme.textColor};
-`;
+import React from 'react';
+import Router from "./Router";
 
 function App() {
 
     return (
         <div>
-            <Container>
-                <H1>Hello</H1>
-            </Container>
+            <Router/>
         </div>
     );
 }
