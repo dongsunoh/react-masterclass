4a21dff85ffdb98203173cff0d318e05c623f161
dongsunoh <dongsun.oh@gmail.com>
2025-07-31 17:48:25 +0900

    Crypto Tracker (Coin Data)  - 브라우저에서 Object.keys(), Object.values() 사용으로 interface 생성하는 법

--- src/routes/Coin.tsx
+++ src/routes/Coin.tsx
@@ -34,13 +34,68 @@ interface RouteState {
     name: string;
 }
 
+interface IInfoData {
+    id: string;
+    name: string;
+    symbol: string;
+    rank: number;
+    is_new: boolean;
+    is_active: boolean;
+    type: string;
+    logo: string;
+    description: string;
+    message: string;
+    open_source: boolean;
+    started_at: string;
+    development_status: string;
+    hardware_wallet: boolean;
+    proof_type: string;
+    org_structure: string;
+    hash_algorithm: string;
+    first_data_at: string;
+    last_data_at: string;
+}
+
+interface IPriceData {
+    id : string;
+    name : string;
+    symbol : string;
+    rank : number;
+    total_supply : number;
+    max_supply : number;
+    beta_value : number;
+    first_data_at : string;
+    last_updated : string;
+    quotes : {
+        USD : {
+            ath_date : string;
+            ath_price: number;
+            market_cap:number;
+            market_cap_change_24h:number;
+            percent_change_1h:number;
+            percent_change_1y:number;
+            percent_change_6h:number;
+            percent_change_7d:number;
+            percent_change_12h:number;
+            percent_change_15m:number;
+            percent_change_24h:number;
+            percent_change_30d:number;
+            percent_change_30m:number;
+            percent_from_price_ath:number;
+            price:number;
+            volume_24h:number;
+            volume_24h_change_24h:number;
+        }
+    };
+}
+
 function Coin () {
 
     const {coinId} = useParams<RouterParams>();
     const [loading, setLoading] = useState(true);
     const {state} = useLocation<RouteState>();
-    const [info, setInfo] = useState({});
-    const [priceInfo, setPriceInfo] = useState({});
+    const [info, setInfo] = useState<IInfoData>();
+    const [priceInfo, setPriceInfo] = useState<IPriceData>();
 
     useEffect(() => {
         (async() => {
@@ -49,6 +104,9 @@ function Coin () {
 
             setInfo(infoData);
             setPriceInfo(priceData);
+
+            console.log(infoData);
+            console.log(priceData);
 
         })();
 
