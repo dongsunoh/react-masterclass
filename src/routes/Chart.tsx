c14c38207ac87e40a978b062788cc53be4baaf9e
dongsunoh <dongsun.oh@gmail.com>
2025-08-02 11:30:03 +0900

    Crypto Tracker (Price Chart part Two )  - APEXCHART lib 사용 (https://apexcharts.com/docs/react-charts/ 사이트 참고)

--- src/routes/Chart.tsx
+++ src/routes/Chart.tsx
@@ -1,5 +1,17 @@
 import {useQuery} from "@tanstack/react-query";
 import {fetchCoinHistory} from "../api";
+import ApexChart from "react-apexcharts";
+
+interface IHistorical {
+    time_open:string;
+    time_close:string;
+    open:number;
+    high:number;
+    low:number;
+    close:number;
+    volume:number;
+    market_cap:number;
+}
 
 interface ChartProps {
     coinId: string;
@@ -7,12 +19,57 @@ interface ChartProps {
 
 function Chart({coinId}:ChartProps) {
 
-    const { isLoading, data } = useQuery({queryKey: ["ohlcv", coinId], queryFn: () => fetchCoinHistory(coinId)});
+    const { isLoading, data } = useQuery<IHistorical[]>({queryKey: ["ohlcv", coinId], queryFn: () => fetchCoinHistory(coinId)});
 
     return (
-        <>
-            <h1>Chart</h1>
-        </>
+        <div>
+            {isLoading ? ("Loading Chart ...") : (
+                <ApexChart
+                    type = "line"
+                    series = {[
+                        {
+                            name : "Price",
+                            data : data?.map((price)=>price.close) as number[],
+                        }
+                    ]}
+                    options = {{
+                        theme :{
+                            mode : "dark"
+                        },
+                        chart : {
+                            height : 300,
+                            width : 500,
+                            toolbar : {
+                                show : false,
+                            },
+                            background : "transparent",
+                        },
+                        grid : {
+                            show : false,
+                        },
+                        stroke : {
+                            curve : "smooth",
+                            width : 5,
+                        },
+                        xaxis : {
+                            axisBorder : {
+                                show : false,
+                            },
+                            axisTicks : {
+                              show : false,
+                            },
+                            labels : {
+                                show : false,
+                            }
+                        },
+                        yaxis : {
+                            show : false,
+                        }
+                    }}
+
+
+            />)}
+        </div>
     );
 }
 
